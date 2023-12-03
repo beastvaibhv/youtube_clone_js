b5f0d317a37af35c707e9f23e7a40fd830ce2894
beastvaibhv <92950515+beastvaibhv@users.noreply.github.com>
2023-12-03 21:49:21 +0530

    youtube clone first commit

---
+++ script.js
@@ -0,0 +1,173 @@
+const bars = document.querySelector(".bars");
+const leftNavBar = document.querySelector(".leftNavBAr");
+const rightVideosCard = document.querySelector(".rightVideosCard");
+
+const search = document.querySelector(".fa-magnifying-glass");
+const SearchBar = document.querySelector(".SearchBar");
+const searchInput = document.querySelector(".searchInput");
+const logoRight = document.querySelector(".logoRight");
+const searchBtn = document.querySelector(".searchBtn");
+const ProfileRight = document.querySelector(".ProfileRight");
+const VideoCardsHolder = document.querySelector(".VideoCardsHolder");
+const thumbImg = document.querySelector(".thumbImg");
+let videoCard;
+const thumbnail = document.querySelector(".thumbnail");
+
+// Hide show Side Navbar Function
+bars.addEventListener("click", hideLeftNav);
+// let mainScreenFlag;
+function hideLeftNav(e) {
+  e.preventDefault();
+  leftNavBar.classList.toggle("hideNav"); //adding a class to hide side nav
+
+  if (leftNavBar.classList.contains("hideNav")) {
+    leftNavBar.style.width = "0";
+    rightVideosCard.style.width = "100%"; //making main screen full when nav hidden
+
+    
+  } else {
+    leftNavBar.style.width = "20%";
+    rightVideosCard.style.width = "80%"; //making main screen 80% when nav visible
+    // mainScreenFlag = false;
+  }
+}
+
+// for mobile view only
+search.addEventListener("click", function (e) {
+  let TargetClass = e.target.classList;
+  // console.log(TargetClass);
+
+  if (TargetClass.contains("fa-magnifying-glass") && width <= 600) {
+    // console.log("Anuj");
+    logoRight.style.display = "none";
+
+    searchInput.style.display = "block";
+    searchInput.style.flexGrow = "0";
+
+    ProfileRight.style.marginRight = "20px";
+
+    SearchBar.style.marginLeft = "20px";
+    SearchBar.style.width = "330px";
+
+    searchBtn.style.width = "55px";
+    searchBtn.style.height = "100%";
+    searchBtn.style.transition = "none";
+    searchBtn.style.backgroundColor = "#303030";
+    searchBtn.style.borderBottomRightRadius = "30px";
+    searchBtn.style.borderTopRightRadius = "30px";
+  } else {
+    // console.log("Outside");
+  }
+});
+
+// WindowsScreen Width Calculating
+let width;
+function updateSizeInfo() {
+  width = window.innerWidth;
+  // document.getElementById('size-info').textContent = `Current window width: ${width}px`;
+  // console.log(`Current window width: ${width}px`);
+  if (width > 600) {
+    // console.log("Bigger Than 600");
+  }
+}
+
+// Call the function initially and on window resize
+window.addEventListener("resize", updateSizeInfo);
+updateSizeInfo();
+
+
+
+// video cards adding dynamically--------------------
+const apiKey = "AIzaSyCOzdiObyNRRonxDD24O20i0TcZY-dth20";
+const baseUrl = "https://www.googleapis.com/youtube/v3";
+
+// THis function will take video id and returns the statics of the video
+
+async function getVideoStatistics(videoId) {
+  // https://www.googleapis.com/youtube/v3/search?key={apiKey}&part=snippet&q=js&type=video
+  const endpoint = `${baseUrl}/videos?key=${apiKey}&part=statistics&id=${videoId}`;
+
+  try {
+    const response = await fetch(endpoint);
+    const result = await response.json();
+    return result.item[0].statistics;
+  } catch (error) {
+    console.log("failed to load statistics");
+  }
+}
+// Calculating Upload time
+function calculateTheTimeGap(publishTime) {
+  let publishDate = new Date(publishTime);
+  let currentDate = new Date();
+
+  let secondsGap = (currentDate.getTime() - publishDate.getTime()) / 1000;
+
+  const secondsPerDay = 24 * 60 * 60;
+  const secondsPerWeek = 7 * secondsPerDay;
+  const secondsPerMonth = 30 * secondsPerDay;
+  const secondsPerYear = 365 * secondsPerDay;
+
+  if (secondsGap < secondsPerDay) {
+    return `${Math.ceil(secondsGap / (60 * 60))}hrs ago`;
+  }
+  if (secondsGap < secondsPerWeek) {
+    return `${Math.ceil(secondsGap / secondsPerWeek)} weeks ago`;
+  }
+  if (secondsGap < secondsPerMonth) {
+    return `${Math.ceil(secondsGap / secondsPerMonth)} months ago`;
+  }
+
+  return `${Math.ceil(secondsGap / secondsPerYear)} years ago`;
+}
+
+function renderVideosOntoUI(videosList) {
+  VideoCardsHolder.innerHTML = "";
+  //videosList will be an array of videos objects
+  videosList.forEach((video) => {
+    videoCard = document.createElement("div");
+    videoCard.className = "videoCard";
+    videoCard.innerHTML = `
+    <div class="thumbnail">
+    <img class="thumbImg" 
+    src="${video.snippet.thumbnails.high.url}"
+     alt="thumbnail" />
+    <p class="timeStamp">5:10</p>
+  </div>
+  <div class="bottomCard">
+    <div class="channelDP">
+      <img src="imgs/User-Avatar-1.png" alt="channelLogo" />
+    </div>
+    <div class="titleDisc">
+      <p class="vidTitle">
+       ${video.snippet.title}
+      </p>
+      <p class="channelName">${video.snippet.channelTitle}</p>
+      <p class="uploadTime">35M views • ${calculateTheTimeGap(video.snippet.publishTime)}</p>
+    </div>
+  </div>`;
+
+    VideoCardsHolder.appendChild(videoCard);
+  });
+}
+
+async function fetchSearchResults(searcString) {
+  //searcString is the search input user entering
+  const endPoint = `${baseUrl}/search?key=${apiKey}&q=${searcString}&part=snippet&maxResults=10`;
+  console.log(`searchValue ${searcString}`);
+  try {
+    const response = await fetch(endPoint);
+    const result = await response.json();
+    console.log(result);
+
+    renderVideosOntoUI(result.items);
+  } catch (error) {
+    alert("10,000 query per day exhausted");
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  const searchValue = searchInput.value;
+  console.log(searchValue);
+  fetchSearchResults(searchValue);
+});
+fetchSearchResults("Ben 10 omnitrix full episodes");
