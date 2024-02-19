e09d9db1f4ac95b72e61660e39037f75ed2fe22c
beastvaibhv <92950515+beastvaibhv@users.noreply.github.com>
2024-02-19 14:57:24 +0530

    adding dark-light mode

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const apiKey="AIzaSyAZrgjenrvAQGEkwm38zdPkBKls_bBwyK0";
+const baseUrl = "https://www.googleapis.com/youtube/v3";
 const bars = document.querySelector(".bars");
 const leftNavBar = document.querySelector(".leftNavBAr");
 const rightVideosCard = document.querySelector(".rightVideosCard");
@@ -12,10 +14,160 @@ const VideoCardsHolder = document.querySelector(".VideoCardsHolder");
 const thumbImg = document.querySelector(".thumbImg");
 let videoCard;
 const thumbnail = document.querySelector(".thumbnail");
+const Items = document.getElementsByTagName("a");
+const videoCardTiles = document.getElementsByClassName("videoCard");
+
+const toggleButton = document.querySelector(".toggle-theme");
+
+
+toggleButton.addEventListener("click", toggleTheme);
+
+function toggleTheme() {
+  const body =document.body;
+  body.classList.toggle('light-theme');
+}
+
+
+// Items.forEach(item=>{
+//   searchQuery = item.textContent;
+  
+//   item.addEventListener("click", ()=>{fetchSearchResults(searchQuery))
+// })
+
+
+
+
+
+//Function for searching the query in the Search bar
+async function fetchSearchResults(searchString) {
+  //searcString is the search input user entering
+  const endPoint = `${baseUrl}/search?key=${apiKey}&q=${searchString}&part=snippet&maxResults=60`;
+  console.log(`searchValue:  ${searchString}`);
+  try {
+    const response = await fetch(endPoint);
+    const result = await response.json();
+    
+
+    renderVideosOntoUI(result.items);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//first initial search
+fetchSearchResults("people are amazing");
+
+// video cards adding dynamically--------------------
+// takes array of data from fetch search result and return array of videoCard element
+function renderVideosOntoUI(videosList) {
+  VideoCardsHolder.innerHTML = "";
+  //videosList will be an array of videos objects
+  videosList.forEach((video) => {
+    videoCard = document.createElement("div");
+    videoCard.addEventListener("click", (event)=>{
+      console.log("clicked")
+       event.preventDefault();
+       const videoIdToSave = video.id.videoId;
+       getVideoStatistics(videoIdToSave);
+       
+       openVideoPage(videoIdToSave, video.snippet.title, video.snippet.channelTitle);
+     },{passive:true})
+    videoCard.className = "videoCard";
+    
+    videoCard.innerHTML = `
+    
+    <div class="thumbnail">
+    <img class="thumbImg" 
+    src="${video.snippet.thumbnails.high.url}"
+     alt="thumbnail" />
+    <p class="timeStamp">5:10</p>
+  </div>
+  <div class="bottomCard">
+    <div class="channelDP">
+      <img src="./assets/images/User.svg" alt="channelLogo" />
+    </div>
+    <div class="titleDisc">
+      <p class="vidTitle">
+       ${video.snippet.title}
+      </p>
+      <p class="channelName">${video.snippet.channelTitle}</p>
+      <p class="uploadTime">35M views • ${calculateTheTimeGap(video.snippet.publishTime)}</p>
+    </div>
+  </div>
+  `;
+
+    VideoCardsHolder.appendChild(videoCard);
+  });
+}
+
+
+// Calculating Upload time that gives days weeks and months etc.
+function calculateTheTimeGap(publishTime) {
+  let publishDate = new Date(publishTime);
+  let currentDate = new Date();
+
+  let secondsGap = (currentDate.getTime() - publishDate.getTime()) / 1000;
+
+  const secondsPerDay = 24 * 60 * 60;
+  const secondsPerWeek = 7 * secondsPerDay;
+  const secondsPerMonth = 30 * secondsPerDay;
+  const secondsPerYear = 365 * secondsPerDay;
+
+  if (secondsGap < secondsPerDay) {
+    return `${Math.ceil(secondsGap / (60 * 60))}hrs ago`;
+  }
+  if (secondsGap < secondsPerWeek) {
+    return `${Math.ceil(secondsGap / secondsPerWeek)} weeks ago`;
+  }
+  if (secondsGap < secondsPerMonth) {
+    return `${Math.ceil(secondsGap / secondsPerMonth)} months ago`;
+  }
+
+  return `${Math.ceil(secondsGap / secondsPerYear)} years ago`;
+}
+
+//Open VideoPage.html
+function openVideoPage(videoId, vidTitle, videoChannelName){
+  //localStorage.setItem("selectedVideoTitle",vidTitle);
+  localStorage.setItem('selectedVideoTitleAndId', JSON.stringify({ title: vidTitle, id: videoId, channelName: videoChannelName }));
+
+  window.location.href = "videoPage.html";
+}
+
+// THis function will take video id and returns the statics of the video
+async function getVideoStatistics(videoId) {
+  
+  const endpoint = `${baseUrl}/videos?key=${apiKey}&part=statistics&id=${videoId}`;
+
+  try {
+    const response = await fetch(endpoint);
+    const result = await response.json();
+    console.log(result.item[0].statistics, "hello");
+    return result.item[0].statistics;
+  } catch (error) {
+    console.log("failed to load statistics");
+  }
+}
+
+// Search for videos which amtch search query
+searchBtn.addEventListener("click", () => {
+  const searchValue = searchInput.value;
+  console.log(searchValue);
+  fetchSearchResults(searchValue);
+});
+
+// for performning search on clicking enter button
+searchInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    const searchValue = searchInput.value;
+    console.log(searchValue);
+    fetchSearchResults(searchValue);
+  }
+});
 
 // Hide show Side Navbar Function
 bars.addEventListener("click", hideLeftNav);
-// let mainScreenFlag;
+ let mainScreenFlag;
 function hideLeftNav(e) {
   e.preventDefault();
   leftNavBar.classList.toggle("hideNav"); //adding a class to hide side nav
@@ -74,134 +226,4 @@ function updateSizeInfo() {
 // Call the function initially and on window resize
 window.addEventListener("resize", updateSizeInfo);
 updateSizeInfo();
-
-
-
-// video cards adding dynamically--------------------
-const apiKey = "AIzaSyDZZXYwbbcNDrORLcYJDhwccxIhr2Lxla8";
-const baseUrl = "https://www.googleapis.com/youtube/v3";
-
-// THis function will take video id and returns the statics of the video
-
-async function getVideoStatistics(videoId) {
-  
-  const endpoint = `${baseUrl}/videos?key=${apiKey}&part=statistics&id=${videoId}`;
-
-  try {
-    const response = await fetch(endpoint);
-    const result = await response.json();
-    console.log(result.item[0].statistics, "hello");
-    return result.item[0].statistics;
-  } catch (error) {
-    console.log("failed to load statistics");
-  }
-}
-// Calculating Upload time
-function calculateTheTimeGap(publishTime) {
-  let publishDate = new Date(publishTime);
-  let currentDate = new Date();
-
-  let secondsGap = (currentDate.getTime() - publishDate.getTime()) / 1000;
-
-  const secondsPerDay = 24 * 60 * 60;
-  const secondsPerWeek = 7 * secondsPerDay;
-  const secondsPerMonth = 30 * secondsPerDay;
-  const secondsPerYear = 365 * secondsPerDay;
-
-  if (secondsGap < secondsPerDay) {
-    return `${Math.ceil(secondsGap / (60 * 60))}hrs ago`;
-  }
-  if (secondsGap < secondsPerWeek) {
-    return `${Math.ceil(secondsGap / secondsPerWeek)} weeks ago`;
-  }
-  if (secondsGap < secondsPerMonth) {
-    return `${Math.ceil(secondsGap / secondsPerMonth)} months ago`;
-  }
-
-  return `${Math.ceil(secondsGap / secondsPerYear)} years ago`;
-}
-
-
-
-async function fetchSearchResults(searchString) {
-  //searcString is the search input user entering
-  const endPoint = `${baseUrl}/search?key=${apiKey}&q=${searchString}&part=snippet&maxResults=60`;
-  console.log(`searchValue:  ${searchString}`);
-  try {
-    const response = await fetch(endPoint);
-    const result = await response.json();
-    
-
-    renderVideosOntoUI(result.items);
-  } catch (error) {
-    
-  }
-}
-
-searchBtn.addEventListener("click", () => {
-  const searchValue = searchInput.value;
-  console.log(searchValue);
-  fetchSearchResults(searchValue);
-});
-
-searchInput.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    const searchValue = searchInput.value;
-    console.log(searchValue);
-    fetchSearchResults(searchValue);
-  }
-});
-fetchSearchResults("people are amazing");
-
-
-function renderVideosOntoUI(videosList) {
-  VideoCardsHolder.innerHTML = "";
-  //videosList will be an array of videos objects
-  videosList.forEach((video) => {
-    videoCard = document.createElement("div");
-    videoCard.addEventListener("click", (event)=>{
-      console.log("clicked")
-       event.preventDefault();
-       const videoIdToSave = video.id.videoId;
-       getVideoStatistics(videoIdToSave);
-       
-       openVideoPage(videoIdToSave, video.snippet.title, video.snippet.channelTitle);
-     },{passive:true})
-    videoCard.className = "videoCard";
-    
-    videoCard.innerHTML = `
-    
-    <div class="thumbnail">
-    <img class="thumbImg" 
-    src="${video.snippet.thumbnails.high.url}"
-     alt="thumbnail" />
-    <p class="timeStamp">5:10</p>
-  </div>
-  <div class="bottomCard">
-    <div class="channelDP">
-      <img src="./assets/images/User.svg" alt="channelLogo" />
-    </div>
-    <div class="titleDisc">
-      <p class="vidTitle">
-       ${video.snippet.title}
-      </p>
-      <p class="channelName">${video.snippet.channelTitle}</p>
-      <p class="uploadTime">35M views • ${calculateTheTimeGap(video.snippet.publishTime)}</p>
-    </div>
-  </div>
-  `;
-
-    VideoCardsHolder.appendChild(videoCard);
-  });
-}
-const videoCardTiles = document.getElementsByClassName("videoCard");
-
-
-
-function openVideoPage(videoId, vidTitle, videoChannelName){
-  //localStorage.setItem("selectedVideoTitle",vidTitle);
-  localStorage.setItem('selectedVideoTitleAndId', JSON.stringify({ title: vidTitle, id: videoId, channelName: videoChannelName }));
-
-  window.location.href = "videoPage.html";
-}
 
